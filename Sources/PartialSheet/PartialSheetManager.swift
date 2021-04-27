1c6d81dab229ad146569402311764ed569ebb9cd
Russ d'Sa <russelldsa@gmail.com>
2021-04-27 15:26:06 -0700

    move back to spring for now

--- Sources/PartialSheet/PartialSheetManager.swift
+++ Sources/PartialSheet/PartialSheetManager.swift
@@ -34,11 +34,16 @@ public class PartialSheetManager: ObservableObject {
     
     /// Possibility to customize the slide in/out animation of the partial sheet
     public var animationCooldown = 0.4
-    //public var defaultShowAnimation: Animation = Animation.spring(response: 0.275, dampingFraction: 0.6, blendDuration: 1.0)
-    public var defaultShowAnimation: Animation = Animation.timingCurve(0.075, 0.82, 0.165, 1.0)
+    
+    //spring animations
+    public var defaultShowAnimation: Animation = Animation.spring(response: 0.4, dampingFraction: 0.7, blendDuration: 1.0)
+    public var defaultKeyboardAnimation: Animation = Animation.spring(response: 0.4, dampingFraction: 0.7, blendDuration: 1.0)
+    
     public var defaultHideAnimation: Animation = Animation.linear(duration: 0.1)
-    //public var defaultKeyboardAnimation: Animation = Animation.spring(response: 0.4, dampingFraction: 0.7, blendDuration: 1.0)
-    public var defaultKeyboardAnimation: Animation = Animation.timingCurve(0.075, 0.82, 0.165, 1.0)
+    
+    //timing curve animations
+    //public var defaultShowAnimation: Animation = Animation.timingCurve(0.075, 0.82, 0.165, 1.0)
+    //public var defaultKeyboardAnimation: Animation = Animation.timingCurve(0.075, 0.82, 0.165, 1.0)
 
     public init() {
         self.content = AnyView(EmptyView())
